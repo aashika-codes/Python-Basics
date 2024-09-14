7e7b761480b2c51ebaf1379af726b7e2e9543c00
aashika-codes <aashikashukla29@gmail.com>
2024-09-14 15:44:30 +0530

    First

---
+++ Print Statement And Comment.py
@@ -0,0 +1,8 @@
+print("welcome to python")
+print("Aashika","Shukla")
+print("3+4=",3+4,)
+print("15-4=",15-4)
+print("15/4=",15/4)
+print("15//4=",15//4)
+print("2 raised to the power 4 is",2**4)
+#comment
