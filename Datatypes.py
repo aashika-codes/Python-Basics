0eec31313ec4cc55d4efac98fe21216276ab1cbd
aashika-codes <aashikashukla29@gmail.com>
2024-09-21 15:30:49 +0530

    1

---
+++ Datatypes.py
@@ -0,0 +1,8 @@
+name="Aashika"
+print("The type of name is",type(name))
+age=23
+print("The type of age is",type(age))
+height=166.7
+print("The type of height is",type(height))
+female=True
+print("The type of female is",type(female))
