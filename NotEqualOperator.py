d9982bf47cbe727672636940c17f18900fe52fc4
aashika-codes <aashikashukla29@gmail.com>
2024-10-05 15:46:30 +0530

    1

---
+++ NotEqualOperator.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the value of a"))
+b=int(input("Enter the value of b"))
+c=int(input("Enter the value of c"))
+if (a!=b) and (a!=c):
+    print("They are different")
+else:
+    print("They are the same")
