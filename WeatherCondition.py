00cc3ce69063c211a12b3afb0c2aef5b63e4c6ec
aashika-codes <aashikashukla29@gmail.com>
2024-11-02 15:21:18 +0530

    1

---
+++ WeatherCondition.py
@@ -0,0 +1,9 @@
+ch=input("Enter A for Autumn and S for spring for the weather forecast")
+def autumn():
+    print("The weather is chilly and rain is at its peak")
+def spring():
+    print("The weather is warm and flowers are blooming")
+if (ch=="A"):
+    autumn()
+else:
+    spring()
