0c4b41665f7eb0fc2aeddad68610bca1ee626fed
aashika-codes <aashikashukla29@gmail.com>
2024-11-09 16:05:30 +0530

    1

---
+++ shutdown.py
@@ -0,0 +1,7 @@
+op=input("do u want to shutdown the computer?")
+def shut():
+    print("your system is shutting down")
+if op=="yes":
+    shut()
+else:
+    print("System is in minimal condition")
