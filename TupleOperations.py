c16409cd3abb2e58ac734f2a68ffe2da21597a76
aashika-codes <aashikashukla29@gmail.com>
2024-12-20 19:54:22 +0530

    1

---
+++ TupleOperations.py
@@ -0,0 +1,9 @@
+tuplex=("tuple", 2,3.6,True)
+print(tuplex)
+tuplex=(6, 3,79,45,2)
+print(tuplex)
+tuplex= tuplex + (9,)
+print(tuplex)
+tuplex=(2,4,7,8,2,9,4,7,3,2)
+_slice=tuplex[3:5]
+print(_slice)
