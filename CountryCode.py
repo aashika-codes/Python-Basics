69ce134e08789f84fc3fcd43d3d379fafccf504d
aashika-codes <aashikashukla29@gmail.com>
2025-01-03 20:11:19 +0530

    1

---
+++ CountryCode.py
@@ -0,0 +1,7 @@
+country_code={'India':'0236',
+                  'Algeria':'7748',
+                  'Vietnam':'3405'}
+print("the country code for India is")
+print(country_code.get('India','Not found'))
+print("the country code for Indonesia is")
+print(country_code.get('Indonesia','Not found'))
