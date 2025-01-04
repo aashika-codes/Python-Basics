380d47668e99022ca094964a223176f5c6b03502
aashika-codes <aashikashukla29@gmail.com>
2025-01-04 15:29:54 +0530

    1

---
+++ IntersectionOfSets.py
@@ -0,0 +1,8 @@
+set_x={"white","orange"}
+set_y={"green","white"}
+print("The original set elements:")
+print(set_x)
+print(set_y)
+print("the intersection of the two sets are:")
+set_z=set_x.intersection(set_y)
+print(set_z)
