818d91fb5d5ea062df1fae9bebaeaf4d52f80881
aashika-codes <aashikashukla29@gmail.com>
2025-01-04 15:24:28 +0530

    1

---
+++ OperationsOnSets.py
@@ -0,0 +1,14 @@
+my_set={1,2,3,4}
+print(my_set)
+my_set={1,"Hey",4.7,(4,6,8)}
+print(my_set)
+my_set={1,2,3,4,1,2}
+print(my_set)
+my_set=set([2,5,8,9,0])
+print(my_set)
+the_set=set([0,1,3,4,5])
+print("Original set:")
+print(the_set)
+the_set.pop()
+print("After removing the first element of the set:")
+print(the_set)
